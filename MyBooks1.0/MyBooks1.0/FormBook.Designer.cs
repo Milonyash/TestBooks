4dbb932ff96c6f243f4ec11882cce589dcf28e07
Milonyash <nastasyamilovanova@mail.ru>
2017-02-01 19:01:49 +0300

    Version 1.0

---
+++ MyBooks1.0/MyBooks1.0/FormBook.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MyBooks1._0
+{
+    partial class FormBook
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtGr = new System.Windows.Forms.DataGridView();
+            this.OpenXML = new System.Windows.Forms.Button();
+            this.SaveXML = new System.Windows.Forms.Button();
+            this.ReportXML = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtGr
+            // 
+            this.dtGr.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGr.Location = new System.Drawing.Point(12, 48);
+            this.dtGr.Name = "dtGr";
+            this.dtGr.Size = new System.Drawing.Size(382, 150);
+            this.dtGr.TabIndex = 0;
+            // 
+            // OpenXML
+            // 
+            this.OpenXML.Location = new System.Drawing.Point(13, 19);
+            this.OpenXML.Name = "OpenXML";
+            this.OpenXML.Size = new System.Drawing.Size(94, 23);
+            this.OpenXML.TabIndex = 1;
+            this.OpenXML.Text = "Открыть XML";
+            this.OpenXML.UseVisualStyleBackColor = true;
+            this.OpenXML.Click += new System.EventHandler(this.OpenXML_Click);
+            // 
+            // SaveXML
+            // 
+            this.SaveXML.Location = new System.Drawing.Point(138, 19);
+            this.SaveXML.Name = "SaveXML";
+            this.SaveXML.Size = new System.Drawing.Size(95, 23);
+            this.SaveXML.TabIndex = 2;
+            this.SaveXML.Text = "Сохранить XML";
+            this.SaveXML.UseVisualStyleBackColor = true;
+            this.SaveXML.Click += new System.EventHandler(this.SaveXML_Click);
+            // 
+            // ReportXML
+            // 
+            this.ReportXML.Location = new System.Drawing.Point(352, 19);
+            this.ReportXML.Name = "ReportXML";
+            this.ReportXML.Size = new System.Drawing.Size(75, 23);
+            this.ReportXML.TabIndex = 3;
+            this.ReportXML.Text = "Отчет XML";
+            this.ReportXML.UseVisualStyleBackColor = true;
+            this.ReportXML.Click += new System.EventHandler(this.ReportXML_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(86, 204);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 4;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(320, 205);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 5;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 241);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.ReportXML);
+            this.Controls.Add(this.SaveXML);
+            this.Controls.Add(this.OpenXML);
+            this.Controls.Add(this.dtGr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtGr)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtGr;
+        private System.Windows.Forms.Button OpenXML;
+        private System.Windows.Forms.Button SaveXML;
+        private System.Windows.Forms.Button ReportXML;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Add;
+    }
+}
+
---
+++ MyBooks1.0/MyBooks1.0/FormBook.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace MyBooks1._0
+{
+    public partial class FormBook : Form
+    {
+        public FormBook()
+        {
+            InitializeComponent();
+        }
+        DataTable dt = null;
+        XDocument xDoc;
+        string name, name1;
+        private DataTable GetDataTableFromDGV(DataGridView dgv)
+        {
+            var dt = new DataTable();
+            foreach (DataGridViewColumn column in dgv.Columns)
+            {
+                if (column.Visible)
+                {
+                    dt.Columns.Add();
+                }
+
+            }
+
+            dt.TableName = "book";
+            dt.Columns[0].ColumnName = "title";
+            dt.Columns[1].ColumnName = "autor";
+            dt.Columns[2].ColumnName = "category";
+            dt.Columns[3].ColumnName = "price";
+            //dt.Columns[4].ColumnName = "year";
+            object[] cellValues = new object[dgv.Columns.Count];
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    cellValues[i] = row.Cells[i].Value;
+                }
+                dt.Rows.Add(cellValues);
+            }
+
+            return dt;
+        }
+        private void SaveXML_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\ПК\Desktop\data.xml";
+            DataTable dT = GetDataTableFromDGV(dtGr);
+            DataSet dS = new DataSet("bookstore");
+            dS.Tables.Add(dT);
+
+            dS.WriteXml(path);
+
+            MessageBox.Show("Записано");
+        }
+
+        private void ReportXML_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            string path = @"C:\Users\ПК\Desktop\data.xml";
+
+            openFileDialog1.Filter = "html files (*.html)|*.html";
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            name1 = openFileDialog1.FileName;
+
+
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+                myStream.Close();
+                XslTransform xslt = new XslTransform();
+                xslt.Load("xslinclude.xsl");
+                XPathDocument xpathdocument = new
+                XPathDocument(path);
+                StreamWriter sw = new StreamWriter(name1);
+                XmlTextWriter writer = new XmlTextWriter(sw);
+                writer.Formatting = Formatting.Indented;
+
+                xslt.Transform(xpathdocument, null, writer, null);
+                MessageBox.Show("Отчет создан");
+            }
+        }
+        private DataTable ReadXml(OpenFileDialog op)
+        {
+            try
+            {
+                XDocument xDoc = XDocument.Load(op.FileName);
+                dt = CreateTable();
+                DataRow newRow = null;
+
+                foreach (XElement elm in xDoc.Element("bookstore").Elements("book"))
+                {
+                    newRow = dt.NewRow();
+                    XAttribute nameAttribute = elm.Attribute("category");
+                    XElement bookAttribute = elm.Element("title");
+                    XElement autorElement = elm.Element("author");
+                    XElement priceElement = elm.Element("price");
+                    if (nameAttribute != null && autorElement != null && priceElement != null)
+                    {
+                        dt.Rows.Add(bookAttribute.Value, autorElement.Value, nameAttribute.Value, priceElement.Value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return dt;
+        }
+        private DataTable CreateTable()
+        {
+            DataTable dt = new DataTable("Books");
+            DataColumn colBook = new DataColumn("Книга", typeof(string));
+            DataColumn colAutor = new DataColumn("Автор", typeof(string));
+            DataColumn colCategory = new DataColumn("Категория", typeof(string));
+            DataColumn colPrice = new DataColumn("Цена", typeof(string));
+
+            dt.Columns.Add(colBook);
+            dt.Columns.Add(colAutor);
+            dt.Columns.Add(colCategory);
+            dt.Columns.Add(colPrice);
+
+
+            return dt;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            int delet = dtGr.SelectedCells[0].RowIndex;
+            dtGr.Rows.RemoveAt(delet);
+        }
+
+        private void OpenXML_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "xml files (*.xml)|*.xml";
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                          name = openFileDialog1.FileName;
+                          xDoc = XDocument.Load(openFileDialog1.FileName);
+                          dtGr.DataSource = ReadXml(openFileDialog1);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
